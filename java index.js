84ca84451dd739708e263c497e74e0a444ac98be
NainaBravo <108770021+NainaBravo@users.noreply.github.com>
2023-02-24 16:44:21 -0300

    Add files via upload

---
+++ java index.js
@@ -0,0 +1,9 @@
+document.getElementById("boton").onclick = function () {
+     console.log ("Capturamos el evento click");
+     document.getElementById("demo").innerHTML="La maestria me proporcionó la formación teórica y práctica que permite ser capaz de resolver situaciones problemáticas complejas en los ámbitos propios de la contabilidad y auditoría.";
+}
+
+document.getElementById("boton1").onclick = function () {
+    console.log ("Capturamos el evento click");
+    document.getElementById("demo1").innerHTML = "Gestionar el estado financiero de una empresa ayuda en la toma de decisiones, permite modificar ciertas estrategias y también definir nuevas metas, es decir, refleja la rentabilidad del negocio. Al mismo tiempo, entrega beneficios como llevar un control estricto del presupuesto, fortalece el control financiero interno de la empresa, permite tomar mejores decisiones financieras y analizar los planes contables y de presupuestos de un negocio. También es clave para demostrar la salud y estabilidad financiera de una empresa frente a stakeholders internos y externos que estén pensando en hacer negocios contigo.";
+}
